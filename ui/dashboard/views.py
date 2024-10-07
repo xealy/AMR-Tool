8134724867d7ea1b1aa0ab8f0215003b9361d82f
Alex C.R <ninjnova4@hotmail.com>
2024-10-07 15:04:01 +1000

    Updates

--- ui/dashboard/views.py
+++ ui/dashboard/views.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 import shutil
 from ultralytics import YOLO
+from PIL import Image
 
 
 bp = Blueprint('main', __name__)
@@ -22,12 +23,18 @@ def index():
         # generate a timestamped folder name
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
         input_folder = f'images/inputs/input_{timestamp}'
+        interim_folder = f'images/interim/interim_{timestamp}'
+        cropped_folder = f'images/cropped/cropped_{timestamp}'
         output_folder = f'images/outputs/output_{timestamp}'
         output_zips_folder = f'images/output_zips/output_{timestamp}'
 
         # create directories if they don't exist
         if not os.path.exists(input_folder):
             os.makedirs(input_folder)
+        if not os.path.exists(interim_folder):
+            os.makedirs(interim_folder)
+        if not os.path.exists(cropped_folder):
+            os.makedirs(cropped_folder)
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
         
@@ -41,11 +48,53 @@ def index():
         
         print('Files successfully uploaded')
 
-        # PREDICT IMAGES WITH MODEL
-        model = YOLO("C:/EGH400-UI/ui/models/ver1.pt")
-        results = model.predict(source=f"C:/EGH400-UI/ui/{input_folder}", show=True)
+        # RAD MODULE
+        rad_model = YOLO("C:/EGH400-UI/ui/models/rad_model.pt")
+        results = rad_model.predict(source=f"C:/EGH400-UI/ui/{input_folder}", show=True)
+        bounding_boxes = {}
         for idx, result in enumerate(results):
-            result.save(filename=f"C:/EGH400-UI/ui/{output_folder}/result_{idx}.jpg")
+            result.save(filename=f"C:/EGH400-UI/ui/{interim_folder}/result_{idx}.jpg")
+
+            key = f'result_{idx}.jpg'
+            bbox_array = result.boxes.xyxy.cpu().numpy()
+            bbox_array_1d = bbox_array.flatten()
+            value = bbox_array_1d
+            bounding_boxes.update({key : value})
+
+        # CROP BASED ON RAD MODULE OUTPUT
+        for filename in os.listdir(f"C:/EGH400-UI/ui/{interim_folder}"):
+            if filename.endswith(".jpg"):
+                image_path = os.path.join(f"C:/EGH400-UI/ui/{interim_folder}", filename)
+                image = Image.open(image_path)
+
+                if filename in bounding_boxes:
+                    bbox = bounding_boxes[filename]
+                    cropped_image = image.crop((bbox[0], bbox[1], bbox[2], bbox[3])) # Crop the image using the bounding box coordinates
+
+                    output_path = os.path.join(f"C:/EGH400-UI/ui/{cropped_folder}", filename) # Save the cropped image to the output folder
+                    cropped_image.save(output_path)
+                    print(f"Cropped image saved to {cropped_folder}")
+                else:
+                    print(f"No bounding box found for {filename}")
+
+        # CR MODULE
+        cr_model = YOLO("C:/EGH400-UI/ui/models/cr_model.pt")
+        results = cr_model.predict(source=f"C:/EGH400-UI/ui/{cropped_folder}", show=True)
+        output_text_file = f"C:/EGH400-UI/ui/{output_folder}/amr_readings.txt"
+        for idx, result in enumerate(results):
+            image_name = f"result_{idx}.jpg"
+            result.save(filename=f"C:/EGH400-UI/ui/{output_folder}/{image_name}")
+
+            # OUTPUT READINGS TO TEXT ??
+            sorted_indices = sorted(range(len(result.boxes.xyxy)), key=lambda i: result.boxes.xyxy[i][0])
+            sorted_class_predictions = [result.boxes.cls[i] for i in sorted_indices]
+            sorted_class_predictions_list = [int(tensor.item()) for tensor in sorted_class_predictions]
+            amr_reading_string = ''.join([str(num) for num in sorted_class_predictions_list])
+
+            print("Sorted class predictions (MSD to LSD):", amr_reading_string)
+
+            with open(output_text_file, 'a') as file:
+                file.write(f"{image_name}: {amr_reading_string}\n")
 
         # compress the output folder into a ZIP file
         zip_filename = f"{output_zips_folder}.zip"
